d6bd4ae8aeecec24c8578cf718ee603f6d888267
Shreyash199811~ <you@example.com>
2023-07-25 14:38:14 +0530

    html-css-js-files

---
+++ HTML.CSS.JS/text.js
@@ -0,0 +1,7 @@
+const greet = () => {
+    const rollno = 112;
+    const name = "Mayank";
+    return rollno +' '+ ' is belong to'+ ' ' + name + ' from IT Department.';
+};
+
+export default greet;
