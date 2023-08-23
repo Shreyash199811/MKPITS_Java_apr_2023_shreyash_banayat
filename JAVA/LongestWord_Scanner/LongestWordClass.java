f953eaa761c2af9145804c5abb0b98a327598f76
Shreyash199811~ <you@example.com>
2023-08-23 22:29:16 +0530

    finding longest word using Scanner

---
+++ JAVA/LongestWord_Scanner/LongestWordClass.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class LongestWordClass {
+    public static void toFindLongestWord(String textFile) throws IOException {
+
+        Scanner scanner = new Scanner(new File(textFile));
+
+        String longestWord=new String();
+
+        while(scanner.hasNext()){
+            String word=scanner.next();
+
+            if(word.length()>longestWord.length()){
+                longestWord=word;
+            }
+        }
+        System.out.println("Longest Word="+longestWord);
+
+    }
+}
+
