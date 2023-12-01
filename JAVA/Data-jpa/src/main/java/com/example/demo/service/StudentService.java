bc6754eaeb35ad3e421ed549bbcec31a38185d81
Shreyash199811~ <you@example.com>
2023-12-01 18:03:00 +0530

    data-JPA

---
+++ JAVA/Data-jpa/src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Student;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentService {
+    List<Student> findAll();
+
+    Student save(Student student);
+    void delete(Integer roll_number);
+
+    Student findParticular(Integer rollNo);
+
+}
---
+++ JAVA/Data-jpa/src/main/java/com/example/demo/service/StudentServiceImp.java
@@ -0,0 +1,44 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.RepositoryJPA;
+import com.example.demo.entity.Student;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImp implements StudentService{
+
+    private RepositoryJPA repositoryJPA;
+
+    @Autowired
+    public StudentServiceImp(RepositoryJPA repositoryJPA) {
+        this.repositoryJPA = repositoryJPA;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return repositoryJPA.findAll();
+    }
+
+    @Transactional
+    @Override
+    public Student save(Student student) {
+        return repositoryJPA.save(student);
+    }
+
+    @Transactional
+    @Override
+    public void delete(Integer roll_number){
+        repositoryJPA.deleteById(roll_number);
+    }
+
+    @Override
+    public Student findParticular(Integer rollNo){
+        Optional<Student> studentOptional=repositoryJPA.findById(rollNo);
+        return studentOptional.get();
+    }
+}
